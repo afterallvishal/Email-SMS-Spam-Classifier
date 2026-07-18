439e6c95a9fad3437f16982fac5b7f71c4e29981
Vishal Shekhar <114502901+afterallvishal@users.noreply.github.com>
2026-07-18 18:33:08 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# Spam Classifier
+
+A Machine Learning based Spam Message Classifier using Natural Language Processing.
+
+## About Project
+
+This project predicts whether a given SMS/Email message is Spam or Not Spam using Machine Learning techniques.
+
+## Technologies Used
+
+- Python
+- NLP
+- Scikit-learn
+- TF-IDF Vectorizer
+- Streamlit
+
+## Features
+
+- Spam and Ham message classification
+- Text preprocessing using NLP techniques
+- User-friendly Streamlit web interface
---
+++ app.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import pickle
+import string
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+import nltk
+
+#load the vectorizer and model
+tfidf=pickle.load(open('vectorizer.pkl','rb'))
+model=pickle.load(open('model.pkl','rb'))
+
+#Initialise stemmer
+ps = PorterStemmer()
+stop_words = set(stopwords.words('english'))   
+
+
+
+#preprocessing function
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stop_words:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+#Steamlit UI
+st.title('Email/SMS Spam Classifier')
+input_sms=st.text_input("Enter the Message")
+
+if st.button('Predict'):
+    #preprocess
+    transformed_sms=transform_text(input_sms)
+
+    #vectorize
+    vector_input=tfidf.transform([transformed_sms])
+
+    #Predict
+    result=model.predict(vector_input)[0]
+
+    if result==1:
+        st.header('Spam!')
+    else:
+        st.header('Not a Spam!')
+
